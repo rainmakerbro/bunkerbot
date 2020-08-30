cdfef68d6dd5b842f124598029d908eff62f43b3
rainmakerbro <1811383+rainmakerbro@users.noreply.github.com>
2020-08-30 16:51:21 -0400

    overhaul to commands finished

--- bunkerbot.py
+++ bunkerbot.py
@@ -19,7 +19,8 @@ import gspread
 gc = gspread.service_account(filename = clientsecretloc)
 
 sh = gc.open("bunkerbot's movielist")
-sh2 = sh.worksheet('Sheet2')
+shmovies = sh.sheet1
+shwatched = sh.worksheet('Watched')
 shgames = sh.worksheet('Games')
 shbooks = sh.worksheet('Books')
 
@@ -35,6 +36,7 @@ async def on_ready():
     await bunkerbot.change_presence(activity=game)
     print('bunkerbot has logged in as {0.user}.'.format(bunkerbot))
 
+#Takes user category and awaits a title message to add to Sheet1
 @bunkerbot.command(aliases=['add', 'new'])
 async def _addtitle(ctx, arg):
     if ctx.author == bunkerbot.user:
@@ -64,13 +66,15 @@ async def _addtitle(ctx, arg):
                 await msg.add_reaction('\U0001F4DA')
                 await msg.add_reaction('\U00002705')
 
+
+#Watched takes a user entered title, removes from the main list,
+#adds to the 'Sheet2' watched list
 @bunkerbot.command(name='watched')
 async def _watched(ctx, *args):
     if ctx.author == bunkerbot.user:
         return
 
     watched_item = ' '.join(args)
-    print(watched_item)
     watched_cells = sh.sheet1.find(watched_item)
     watched_row = watched_cells.row
     sh.sheet1.delete_rows(watched_row)
@@ -78,13 +82,25 @@ async def _watched(ctx, *args):
     await ctx.add_reaction('\U00002705')
     await ctx.add_reaction('\U0001F440')
 
-@bunkerbot.command(aliases=['list', 'library', 'movies', 'games', 'books'])
+#Displays full list of designated type in private message
+@bunkerbot.command(aliases=['movies', 'games', 'books'])
 async def _list(ctx):
     if ctx.author == bunkerbot.user:
         return
 
-    values = f'{arg}\n '.join(map(str, (sh.sheet1.col_values(1)))) #Change sheet based on ctx?
-    await message.channel.send(values)
+    if ctx.invoked_with == 'movies':
+        chosensheet = 'sh' + ctx.invoked_with
+        values = f'\n '.join(map(str, (shmovies.col_values(1))))
+    if ctx.invoked_with == 'games':
+        chosensheet = 'sh' + ctx.invoked_with
+        values = f'\n '.join(map(str, (shgames.col_values(1))))
+    if ctx.invoked_with == 'books':
+        chosensheet = 'sh' + ctx.invoked_with
+        values = f'\n '.join(map(str, (shbooks.col_values(1))))
+
+    listtype = str.capitalize(ctx.invoked_with)
+    await ctx.author.send(f'{listtype} list: \n')
+    await ctx.author.send(values)
 
 @bunkerbot.command(aliases=['remove', 'old', 'burn'])
 async def _remove(ctx, *args):
